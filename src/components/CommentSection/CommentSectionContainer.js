e8b4a49034fceec8344b640ee8e2e406eab1172d
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-03-01 21:05:37 -0500

    Comments added, but all comments load for both posts instead of their own individual ones

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import dummyData from "/Users/blakedavis/GitRepositories/React-Components-Insta-Clone/src/dummy-data.js";
 
 const CommentSection = props => {
   // Add state for the comments
@@ -11,6 +12,11 @@ const CommentSection = props => {
     <div>
       {/* map through the comments data and return the Comment component */}
       <CommentInput />
+      {dummyData.map(x => 
+        x.comments.map(y => {
+          return (<Comment comment={y}/>)
+        }))
+      }
     </div>
   );
 };
