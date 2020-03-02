ee43a64a49fe1bd0c22e7c3be8f1f4a9eee4ab09
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-03-01 20:25:39 -0500

    Posts are now appearing on the page with the map method

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -4,12 +4,17 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from "/Users/blakedavis/GitRepositories/React-Components-Insta-Clone/src/dummy-data.js";
 
 const PostsPage = () => {
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {dummyData.map(x => {
+        return <Post post={x}/>
+      })}
+  
     </div>
   );
 };
